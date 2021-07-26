9c8ef18628d60bc1990bf67b1dc0d998905c5205
Helmy-chaabane <Chaabane504@gmail.com>
2021-07-26 19:27:08 +0100

    changes

--- front/front-react/src/Components/Container/Container.jsx
+++ front/front-react/src/Components/Container/Container.jsx
@@ -5,20 +5,7 @@ import "./container.css";
 const Container = () => {
   return (
     <div className="main_container">
-      <div class="item">
-<<<<<<< HEAD
-        <h2 className="item_title item__title_left">Albums</h2>
-        <CarouselImg className="carousel_div" />
-=======
-        <Slider />
->>>>>>> e6d751aa734138a31641e415d9164b632332f49e
-      </div>
-      <div class="item">
-        <Slider />
-      </div>
-      <div class="item">
-        <Slider />
-      </div>
+      <div class="item"></div>
       <div class="item">
         <Slider />
       </div>
